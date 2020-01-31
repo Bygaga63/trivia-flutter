f5ed423679e53ed7af0de2b883c7198c0fb50abd
bygaga63 <Bygaga63>
2020-01-31 17:57:14 +0300

    add bloc

---
+++ lib/features/number_trivia/presentation/manager/number_trivia_event.dart
@@ -0,0 +1,21 @@
+import 'package:equatable/equatable.dart';
+
+abstract class NumberTriviaEvent extends Equatable {
+}
+
+class GetTriviaForConcreteNumber extends NumberTriviaEvent {
+  final String numberString;
+
+  GetTriviaForConcreteNumber(this.numberString);
+
+  @override
+  List<Object> get props => [numberString];
+
+
+}
+
+class GetTriviaForRandomNumber extends NumberTriviaEvent {
+
+  @override
+  List<Object> get props => [];
+}
